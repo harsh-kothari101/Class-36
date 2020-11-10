fee68bbd273b088f52c0b666b025694350dfd497
harsh-kothari101 <72602671+harsh-kothari101@users.noreply.github.com>
2020-11-10 20:27:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+var playerCount;
+
+var form,game,player;
+
+var gameState = 0
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+
+}
+
+function draw(){
+  
+  
+ 
+  
+}
+
